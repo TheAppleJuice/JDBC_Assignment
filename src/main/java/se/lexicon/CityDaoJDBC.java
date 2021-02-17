f8bf1acba94da0547a87bbdbfedb3a90bcf4308c
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-02-17 11:08:28 +0100

    implemented methods from interface

--- src/main/java/se/lexicon/CityDaoJDBC.java
+++ src/main/java/se/lexicon/CityDaoJDBC.java
@@ -1,7 +1,58 @@
 package se.lexicon;
 
 
-public class CityDaoJDBC {
+import se.lexicon.dao.CityDao;
+import se.lexicon.model.City;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
 
+public class CityDaoJDBC implements CityDao {
+
+    public static Connection mySqlConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "12345678");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    @Override
+    public City findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByCode(String Code) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<City> findAll() {
+        return null;
+    }
+
+    @Override
+    public City add(City city) {
+        return null;
+    }
+
+    @Override
+    public City update(City city) {
+        return null;
+    }
+
+    @Override
+    public int delete(City city) {
+        return 0;
+    }
 }
